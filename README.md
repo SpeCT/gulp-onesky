a755c3943005df9a1ed29186b8701c45bc94f920
Yury Proshchenko <spect.man@gmail.com>
2016-05-23 16:49:49 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# gulp-onesky [![Build Status](https://travis-ci.org/spect/gulp-onesky.svg?branch=master)](https://travis-ci.org/spect/gulp-onesky)
+
+> My classy gulp plugin
+
+
+## Install
+
+```
+$ npm install --save-dev gulp-onesky
+```
+
+
+## Usage
+
+```js
+var gulp = require('gulp');
+var onesky = require('gulp-onesky');
+
+gulp.task('default', () => {
+  onesky({
+    publicKey: '<onesky account public key>',
+    secretKey: '<onesky account secret key>',
+    projectId: '<your project id>',
+    sourceFile: '<onesky source filename>'
+  }))
+  .pipe(gulp.dest('src'))
+);
+```
+
+
+## License
+
+MIT © [Iurii Proshchenko](https://github.com/spect)
---
+++ index.js
@@ -0,0 +1,63 @@
+'use strict';
+var crypto = require('crypto');
+var querystring = require('querystring');
+
+var gutil = require('gulp-util');
+var request = require('request');
+var through = require('through');
+
+module.exports = function (opts) {
+  opts = opts || {};
+
+  if (!opts.publicKey || !opts.secretKey)
+    throw new gutil.PluginError('gulp-onesky', 'please specify public and secret keys');
+
+  if (!opts.projectId)
+    throw new gutil.PluginError('gulp-onesky', 'please specify project id');
+
+  if (!opts.sourceFile)
+    throw new gutil.PluginError('gulp-onesky', 'please specify source file name');
+
+  if (!opts.outputFile)
+    opts.outputFile = opts.sourceFile + '.i18n.json';
+
+  var time = Math.floor(Date.now() / 1000);
+  var hash = crypto.createHash('md5').update('' + time + opts.secretKey).digest('hex');
+  var url = 'https://platform.api.onesky.io/1/projects/' + opts.projectId;
+  url += '/translations/multilingual?' + querystring.stringify({
+    'api_key': opts.publicKey,
+    'timestamp': time,
+    'dev_hash': hash,
+    'file_format': 'I18NEXT_MULTILINGUAL_JSON',
+    'source_file_name': opts.sourceFile
+  });
+
+  var stream = through(function (file, enc, cb) {
+    this.push(file);
+    cb();
+  });
+
+  request({ url: url, encoding: null, json: true }, function (err, res, body) {
+    if (err) {
+      stream.emit('error', new gutil.PluginError('gulp-onesky', err.message || err.toString()));
+      return;
+    }
+
+    if (body.meta) {
+      if (body.meta.status !== 200) {
+        stream.emit('error', new gutil.PluginError('gulp-onesky', body.meta.message));
+        return;
+      } else {
+        body = body.meta.data;
+      }
+    }
+
+    stream.queue(new gutil.File({
+      path: opts.outputFile,
+      contents: new Buffer(JSON.stringify(body, null, 2))
+    }));
+    stream.emit('end');
+  });
+
+  return stream;
+};
